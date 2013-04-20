89be682326c2b951cf4d058d62f3c49208071352
Matthias Reining <>
2013-04-21 00:47:50 +0200

    some sample parameterized tests

--- src/test/java/com/jexunit/base/MyComplexService.java
+++ src/test/java/com/jexunit/base/MyComplexService.java
@@ -7,7 +7,8 @@ public class MyComplexService {
 	public double calculateFees(double number) {
 		BigDecimal bd = new BigDecimal(number);
 		bd = bd.multiply(new BigDecimal("0.1"));
-		//return number*0.1; <-- floating arithmetic
+                // floating arithmetic problem
+		//return number*0.1; 
 		return  bd.doubleValue();
 	}
 
